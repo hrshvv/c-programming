84c2ffb290a69bb284ffe1a930fa75fb62e908e5
hrshvv <harshverma1022006@gmail.com>
2024-09-05 19:21:54 +0530

    first comit

---
+++ p1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int l,b;
+    printf("Enter Length of the Rectangle");
+    scanf("%d", &l);
+
+    printf("Enter Breadth of the Rectangle");
+    scanf("%d", &b);
+
+    printf("The Area of the Rectangle is %d", l*b);
+    return 0;
+}
---
+++ p3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int r ,h;
+    printf("Enter Radius of the Circle");
+    scanf("%d", &r);
+
+    printf("Area of the Circle with %d is %f", r, 3.14*r*r);
+    return 0;
+}
